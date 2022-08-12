9d52d546fdd165466f6ad3a42dbce7158799329a
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-08-12 17:35:54 +0100

    openmp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 #include <iomanip>
 #include <chrono>
 
+#ifdef _OPENMP
+#include "omp.h"
+#endif
+
 namespace cst{
 
-constexpr size_t Nx = 41;
+constexpr size_t Nx = 101;
 constexpr size_t Ny = Nx;
 constexpr size_t N = Nx * Ny;
 
@@ -17,8 +21,8 @@ constexpr auto dx = static_cast<double>(Lx) / (Nx - 1);
 constexpr auto dy = static_cast<double>(Ly) / (Ny - 1);
 
 constexpr auto Nt = 50;
-constexpr auto iters = 700;
-constexpr auto dt = 0.001;
+constexpr auto iters = 7000;
+constexpr auto dt = 0.0001;
 
 constexpr auto rho = 1.0;
 constexpr auto nu = 0.1;
@@ -52,6 +56,9 @@ inline auto multiindex(size_t i, size_t j){
 void write(const std::string& fname, const Grid& g){
     std::ofstream outf{fname};
     if(outf.is_open()){
+        outf << "# Lx " << cst::Lx << "\n# Ly " << cst::Ly << '\n';
+        outf << "# Nx " << cst::Nx << "\n# Ny " << cst::Ny << '\n'; 
+
         for(size_t iy = 0; iy < cst::Ny; iy++){
             for(size_t ix = 0; ix < cst::Ny; ix++){
                 auto idx = index(iy, ix);
@@ -144,31 +151,31 @@ void step(Grid& u_new, Grid& v_new, Grid& p, const Grid& u, const Grid& v){
                             (v[idx_up] - 2.0 * v[idx] + v[idx_down]);
         }
     }
-}
-
-void boundary(Grid& u, Grid& v){
+    
     for(size_t iy = 0; iy < cst::Ny; iy++){
         auto x_left = util::index(iy, 0);
         auto x_right = util::index(iy, cst::Nx - 1);
-        u[x_left] = 0.0;
-        u[x_right] = 0.0;
-        v[x_left] = 0.0;
-        v[x_right] = 0.0;
+        u_new[x_left] = 0.0;
+        u_new[x_right] = 0.0;
+        v_new[x_left] = 0.0;
+        v_new[x_right] = 0.0;
     }
 
     for(size_t ix = 0; ix < cst::Nx; ix++){
         auto y_top = util::index(0, ix);
         auto y_bottom = util::index(cst::Ny - 1, ix);
-        u[y_bottom] = 1.0;
-        u[y_top] = 0.0;
-        v[y_bottom] = 0.0;
-        v[y_top] = 0.0;
+        u_new[y_bottom] = 1.0;
+        u_new[y_top] = 0.0;
+        v_new[y_bottom] = 0.0;
+        v_new[y_top] = 0.0;
     }
 }
 
 void run(){
     Grid u, v, p, u_new, v_new;
 
+    auto ti = std::chrono::high_resolution_clock::now();
+    
     for(size_t idx = 0; idx < cst::N; idx++){
         u[idx] = 0.0;
         v[idx] = 0.0;
@@ -178,34 +185,194 @@ void run(){
     }
 
     for(size_t it = 0; it < cst::iters; it++){
-        serial::step(u_new, v_new, p, u, v);
-        serial::boundary(u_new, v_new);
+        step(u_new, v_new, p, u, v);
 
         std::copy(std::cbegin(u_new), std::cend(u_new), std::begin(u));
         std::copy(std::cbegin(v_new), std::cend(v_new), std::begin(v));
     }
 
-    util::write("p.dat", p);
-    util::write("u.dat", u);
-    util::write("v.dat", v);
+    auto tf = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(tf - ti);
+    std::cout << "serial::run [1]\t\t" << duration.count() << " ms\n";
+
+    util::write("p_serial.dat", p);
+    util::write("u_serial.dat", u);
+    util::write("v_serial.dat", v);
 }
 
 } // serial
 
-int main(){
-    std::cout << "+++ CFD +++" << '\n';
-    std::cout << "Lx = " << cst::Lx << '\n' << "Ly = " << cst::Ly << '\n';
-    std::cout << "Nx = " << cst::Nx << '\n' << "Ny = " << cst::Ny << '\n';
-    std::cout << "dx = " << cst::dx << '\n' << "dy = " << cst::dy << '\n';
-    std::cout << "dt = " << cst::dt << '\n';
-    std::cout << "+++ +++ +++\n\n";
+namespace omp{
+
+void pressure(Grid& p, const Grid& u, const Grid& v){
+    Grid c;
+
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for(size_t iy = 1; iy < cst::Ny - 1; iy++){ // y runs across rows
+            #pragma omp simd
+            for(size_t ix = 1; ix < cst::Nx - 1; ix++){ // x runs across columns
+                auto [idx, idx_right, idx_left, idx_up, idx_down] = util::multiindex(iy, ix);
+                
+                auto du_x = (u[idx_right] - u[idx_left]) / (2.0 * cst::dx);
+                auto du_y = (u[idx_up] - u[idx_down]) / (2.0 * cst::dy);
+                auto dv_x = (v[idx_right] - v[idx_left]) / (2.0 * cst::dx);
+                auto dv_y = (v[idx_up] - v[idx_down]) / (2.0 * cst::dy); 
+                
+                c[idx] = - cst::rho * cst::dx2 * cst::dy2 / cst::dx2dy2 *
+                        (
+                            (du_x + dv_y) / cst::dt
+                            - du_x * du_x - 2.0 * du_y * dv_x - dv_y * dv_y
+                        );
+            }
+        }
+
+        for(size_t i = 0; i < cst::Nt; i++){
+            Grid pold;
+
+            #pragma omp for simd
+            for(size_t j = 0; j < cst::N; j++){
+                pold[j] = p[j];
+            }
+
+            #pragma omp for
+            for(size_t iy = 1; iy < cst::Ny - 1; iy++){ // y runs across rows
+                #pragma omp simd
+                for(size_t ix = 1; ix < cst::Nx - 1; ix++){ // x runs across columns
+                    auto [idx, idx_right, idx_left, idx_up, idx_down] = util::multiindex(iy, ix);
+
+                    p[idx] = (
+                        (pold[idx_right] + pold[idx_left]) * cst::dy2 +
+                        (pold[idx_up] + pold[idx_down]) * cst::dx2
+                        ) / cst::dx2dy2 + c[idx];
+                }
+            }
+
+            #pragma omp for simd nowait
+            for (size_t iy = 0; iy < cst::Ny; iy++) {
+                p[util::index(iy, 0)] = p[util::index(iy, 1)];
+                p[util::index(iy, cst::Nx - 1)] = p[util::index(iy, cst::Nx - 2)];
+            }
+
+            #pragma omp for simd
+            for (size_t ix = 0; ix < cst::Nx; ix++) {
+                p[util::index(0, ix)] = p[util::index(1, ix)];
+                p[util::index(cst::Ny - 1, ix)] = 0.0;
+            }
+        }
+    }
+}
+
+void step(Grid& u_new, Grid& v_new, Grid& p, const Grid& u, const Grid& v){
+    pressure(p, u, v);
+
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (size_t iy = 1; iy < cst::Ny - 1; iy++) {   // y runs across rows
+            #pragma omp simd
+            for (size_t ix = 1; ix < cst::Nx - 1; ix++) { // x runs across columns
+                auto [idx, idx_right, idx_left, idx_up, idx_down] =
+                    util::multiindex(iy, ix);
+
+                u_new[idx] =
+                    u[idx] - u[idx] * cst::dt / cst::dx * (u[idx] - u[idx_left]) -
+                    v[idx] * cst::dt / cst::dy * (u[idx] - u[idx_down]) -
+                    cst::dtDdx / (2.0 * cst::rho) * (p[idx_right] - p[idx_left]) +
+                    cst::nu * cst::dtDdx / cst::dx *
+                        (u[idx_right] - 2.0 * u[idx] + u[idx_left]) +
+                    cst::nu * cst::dtDdy / cst::dy *
+                        (u[idx_up] - 2.0 * u[idx] + u[idx_down]);
+
+                v_new[idx] = v[idx] -
+                            u[idx] * cst::dt / cst::dx * (v[idx] - v[idx_left]) -
+                            v[idx] * cst::dt / cst::dy * (v[idx] - v[idx_down]) -
+                            cst::dtDdy / (2.0 * cst::rho) * (p[idx_up] - p[idx_down]) +
+                            cst::nu * cst::dtDdx / cst::dx *
+                                (v[idx_right] - 2.0 * v[idx] + v[idx_left]) +
+                            cst::nu * cst::dtDdy / cst::dy *
+                                (v[idx_up] - 2.0 * v[idx] + v[idx_down]);
+            }
+        }
+
+        #pragma omp for simd nowait
+        for(size_t iy = 0; iy < cst::Ny; iy++){
+            auto x_left = util::index(iy, 0);
+            auto x_right = util::index(iy, cst::Nx - 1);
+            u_new[x_left] = 0.0;
+            u_new[x_right] = 0.0;
+            v_new[x_left] = 0.0;
+            v_new[x_right] = 0.0;
+        }
+
+        #pragma omp for simd nowait
+        for(size_t ix = 0; ix < cst::Nx; ix++){
+            auto y_top = util::index(0, ix);
+            auto y_bottom = util::index(cst::Ny - 1, ix);
+            u_new[y_bottom] = 1.0;
+            u_new[y_top] = 0.0;
+            v_new[y_bottom] = 0.0;
+            v_new[y_top] = 0.0;
+        }
+    }
+}
+
+void run(){
+    Grid u, v, p, u_new, v_new;
+
+    int n_omp_threads = -1;
+#ifdef _OPENMP
+    #pragma omp parallel
+    {
+        n_omp_threads = omp_get_num_threads();
+    }
+#endif
 
     auto ti = std::chrono::high_resolution_clock::now();
-    serial::run();
+
+    #pragma omp parallel for simd
+    for(size_t idx = 0; idx < cst::N; idx++){
+        u[idx] = 0.0;
+        v[idx] = 0.0;
+        p[idx] = 0.0;
+        u_new[idx] = 0.0;
+        v_new[idx] = 0.0;
+    }
+
+    for(size_t it = 0; it < cst::iters; it++){
+        step(u_new, v_new, p, u, v);
+
+        #pragma omp parallel for simd
+        for(size_t i = 0; i < cst::N; i++){
+            u[i] = u_new[i];
+            v[i] = v_new[i];
+        }
+    }
+
     auto tf = std::chrono::high_resolution_clock::now();
     auto duration =
         std::chrono::duration_cast<std::chrono::milliseconds>(tf - ti);
-    std::cout << "serial::run\t\t" << duration.count() << " ms\n";
+    std::cout << "omp::run [" << n_omp_threads << "]\t\t" << duration.count() << " ms\n";
+    
+    util::write("p_omp.dat", p);
+    util::write("u_omp.dat", u);
+    util::write("v_omp.dat", v);
+}
+
+} // omp
+
+int main(){
+    std::cout << "+++ Cavity Flow +++" << '\n';
+    std::cout << "Lx = " << cst::Lx << '\t' << "Ly = " << cst::Ly << '\n';
+    std::cout << "Nx = " << cst::Nx << '\t' << "Ny = " << cst::Ny << '\n';
+    std::cout << "dx = " << cst::dx << '\t' << "dy = " << cst::dy << '\n';
+    std::cout << "dt = " << cst::dt << '\n';
+    std::cout << "+++ ----------- +++\n\n";
+
+    serial::run();
+    omp::run();
 
     return 0;
 }
--- plot.py
+++ plot.py
@@ -4,12 +4,19 @@ import sys
 from matplotlib import pyplot as plt
 from matplotlib import cm
 
+with open(sys.argv[1]) as f:
+    Lx = float(f.readline().split()[-1])
+    Ly = float(f.readline().split()[-1])
+    Nx = int(f.readline().split()[-1])
+    Ny = int(f.readline().split()[-1])
+
 p = np.loadtxt(sys.argv[1])
 u = np.loadtxt(sys.argv[2])
 v = np.loadtxt(sys.argv[3])
+name = sys.argv[4]
 
-x = np.linspace(0, 2, 41)
-y = np.linspace(0, 2, 41)
+x = np.linspace(0.0, Lx, Nx)
+y = np.linspace(0.0, Lx, Ny)
 
 X, Y = np.meshgrid(x, y, indexing="xy")
 
@@ -18,4 +25,4 @@ plt.colorbar()
 plt.contour(X, Y, p, cmap=cm.viridis)
 plt.streamplot(X, Y, u, v)
 
-plt.savefig("cavity.png")
+plt.savefig(f"cavity-{name}.png")
